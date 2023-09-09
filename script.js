081515a205b3d3d65b75c8f04b327784a8743839
Tulasijoshua <mbawinitulasi12@gmail.com>
2023-09-09 15:04:19 +0000

    Feat: finalizing

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let currentDay = document.getElementById("currentDay");
-let currentTime = document.getElementById("currentTime");
+let currentDay = document.querySelector("[data-testid=currentDayOfTheWeek]");
+let currentTime = document.querySelector("[data-testid=currentUTCTime]");
 
 let currentDatetime = new Date();
 
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const dayOfWeek = daysOfWeek[currentDatetime.getDay()];
 
-let hours = currentDatetime.getHours();
+let hours = currentDatetime.getUTCHours();
 const ampm = hours >= 12 ? 'PM' : 'AM';
-hours = hours % 12 || 12;
-const minutes = currentDatetime.getMinutes();
-const seconds = currentDatetime.getSeconds();
+// hours = hours % 12 || 12;
+const minutes = currentDatetime.getUTCMinutes();
+const seconds = currentDatetime.getUTCSeconds();
 
+const hrs = hours < 10 ? `0${hours}` : `${hours}`
 const sec = seconds < 10 ? `0${seconds}` : `${seconds}`
 const min = minutes < 10 ? `0${minutes}` : `${minutes}`
 
-// const datetimeElement = document.getElementById("datetime");
 currentDay.textContent = `${dayOfWeek}`;
-currentTime.textContent =`${hours}:${minutes}:${sec} ${ampm}`;
+currentTime.textContent =`${hrs}:${min}:${sec} ${ampm}`;
